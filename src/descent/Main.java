3d006dc3242bf8103a1e673e0440e855a8a9eda5
Taeshinobu <kobe.laprade@gmail.com>
2020-10-12 22:47:52 -0400

    code :100:
    lol

---
+++ src/descent/Main.java
@@ -0,0 +1,16 @@
+package descent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+    @Override
+    public void onEnable() {
+    	
+    }
+    @Override
+    public void onDisable() {
+
+    }
+    
+}
