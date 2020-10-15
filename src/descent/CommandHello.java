ea8d5bceadd2755de4f35dd44e0fb1a6e01c014a
Zachary Montgomery <zsm5532@rit.edu>
2020-10-15 18:34:36 -0400

    stuff

--- src/descent/CommandHello.java
+++ src/descent/CommandHello.java
@@ -4,16 +4,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class CommandHello implements CommandExecutor {
-
-    // This method is called, when somebody uses our command
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    	if (sender instanceof Player) {
-            Bukkit.broadcastMessage("Hello");
-        }
+        Bukkit.broadcastMessage("Hello");
         return true;
     }
 }
--- src/descent/DescentListener.java
+++ src/descent/DescentListener.java
@@ -1,13 +1,31 @@
 package descent;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class DescentListener implements Listener{
 	@EventHandler
     public void onPlayerJoin(PlayerJoinEvent event)
     {
 		event.setJoinMessage("Welcome, " + event.getPlayer().getName() + ", to the Descent Project!");
+    }
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event)
+    {
+		event.setQuitMessage("Goodbye, " + event.getPlayer().getName() + "...");
+    }
+	@EventHandler
+	public void onRightClick(PlayerInteractEvent event)
+    {
+		if (event.getItem() != null && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && event.getItem().getType() == Material.BOW) {
+			Bukkit.broadcastMessage("True");
+		
+		}
     }
 }
